5b77986215d5843a5b8dac51e1ccc0934886213b
4Bee <109135643+4BFC@users.noreply.github.com>
2023-02-24 00:53:31 +0900

    1st_commit
    1st_commit

---
+++ JSproject/text_Adventure/Js/clock.js
@@ -0,0 +1,20 @@
+const clock = document.querySelector("#clock");
+const day = document.querySelector("#day");
+let daycount = 0;
+//시계 만들기
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+  if (seconds === "00") {//===는 타입까지 맞아야한다.
+    console.log("quest!!");
+    daycount += 1;
+    day.innerText = `Day : ${daycount}`;
+  }
+}
+console.log(getClock());
+setInterval(getClock, 1000);
---
+++ JSproject/text_Adventure/Js/story.js
@@ -0,0 +1,17 @@
+const story = document.querySelector("#story"); //스토리 내용 기입
+const quest = document.querySelector("#quest"); //퀘스트 내용 기입
+const dice = document.querySelector("#dice"); //주사위 숫자 기입
+//게임 진행 조건
+//day = 5 퀘스트 생성
+//day = 1 작업 생성
+const day = document.querySelector("#day");
+//버튼
+const attack = document.querySelector("#atk");
+const run = document.querySelector("#run");
+const buy = document.querySelector("#buy");
+const sale = document.querySelector("#sale");
+
+
+//랜덤으로 주사위 만들기
+
+
