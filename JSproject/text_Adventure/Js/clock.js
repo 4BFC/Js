74ae0f00ec05b077436c9f2fc23ddc7f74f0c9ea
4Bee <109135643+4BFC@users.noreply.github.com>
2023-02-27 23:24:28 +0900

    3rd_commit
    3rd_commit

--- JSproject/text_Adventure/Js/clock.js
+++ JSproject/text_Adventure/Js/clock.js
@@ -1,13 +1,26 @@
+//시간
 const clock = document.querySelector("#clock");
 const day = document.querySelector("#day");
 const time = document.querySelector("#time");
-let daycount = 0;
-//시계 만들기
-function getClock() {
+
+//시나리오
+const story = document.querySelector("#story"); //스토리 내용 기입
+const quest = document.querySelector("#quest"); //퀘스트 내용 기입
+
+//버튼
+const attack = document.querySelector("#atk");
+const run = document.querySelector("#run");
+const buy = document.querySelector("#buy");
+const sale = document.querySelector("#sale");
+
+//시계 만들기 -> update()
+function update() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  let daycount = 1;
 
   clock.innerText = `${hours}:${minutes}:${seconds}`;
   time.innerText = `Time : ${seconds}`;
@@ -17,8 +30,16 @@ function getClock() {
     daycount += 1;
     day.innerText = `Day : ${daycount}`;
   }
+
+  if ((daycount % 2) === 0) {
+    story.innerText = "노동하기";
+  } else if (!(daycount % 2) === 0) {
+    story.innerText = "NOT YET!";
+    console.log("NOT YET");
+  }
+
 }
 
-console.log(getClock());
+console.log(update());
 
-setInterval(getClock, 1000);
+setInterval(update, 1000);
---
+++ JSproject/text_Adventure/Js/dice.js
@@ -0,0 +1,34 @@
+// const story = document.querySelector("#story"); //스토리 내용 기입
+// const quest = document.querySelector("#quest"); //퀘스트 내용 기입
+const dice = document.querySelector("#dice"); //주사위 숫자 기입
+const dic_btn = document.querySelector("#dic_btn");
+
+//게임 진행 조건
+//day = 5 퀘스트 생성
+//day = 1 작업 생성
+
+//clock요소 가져오는 변수
+// const isday = document.querySelector("#day");
+
+// //버튼
+// const attack = document.querySelector("#atk");
+// const run = document.querySelector("#run");
+// const buy = document.querySelector("#buy");
+// const sale = document.querySelector("#sale");
+
+// if ((Number(isday) % 2) === 0) {
+//   console.log("노동하기");
+// } else { console.log("-"); }
+
+//랜덤으로 주사위 만들기
+function dicePlay() {
+  console.log("check");
+  //dice의 여러 상태의 조건에따라 실행이 되고 안되고를 만들어야한다.
+  let num = Math.floor((Math.random() * 9) + 1);
+  dice.innerText = `Dice : ${num}`;
+
+}
+
+dic_btn.addEventListener("click", dicePlay);
+
+
