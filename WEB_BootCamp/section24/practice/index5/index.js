c4d7a75e10f62e2cc7d896347a53ed87cbed33cd
4Bee <109135643+4BFC@users.noreply.github.com>
2023-03-25 17:45:04 +0900

    2nd_commit
    2nd_commit

---
+++ WEB_BootCamp/section24/practice/index5/index.js
@@ -0,0 +1,6 @@
+let container = document.querySelector('#container');
+let img = document.querySelector('img');
+
+container.style.textAlign = 'center';
+img.style.width = '150px';
+img.style.borderRadius = '50%';
