7b77a032fdb1540b9cad43dc5b9553f3f5a105e5
4Bee <109135643+4BFC@users.noreply.github.com>
2023-05-11 23:01:50 +0900

    31th_commit
    31th_commit

---
+++ WEB_BootCamp/000_dreamCode/array/array.js
@@ -0,0 +1,12 @@
+//1.Declaration
+const arr1 = new Array();
+const arr2 = [1, 2];
+
+//2. index position
+//Looping over an array
+//a. for
+//b. for of
+//c.forEach()
+//4.Addtion, deletion, copy
+//opush add an item to the end
+//+. splice
