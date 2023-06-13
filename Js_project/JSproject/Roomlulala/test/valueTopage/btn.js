a29bbd6ec94ef2fc5dc0f5311347dbea03e3fe3c
4BFC <4bee.code@gmail.com>
2023-06-13 12:41:36 +0900

    create new index.html local.js files& edited btn.js

--- Js_project/JSproject/Roomlulala/test/valueTopage/btn.js
+++ Js_project/JSproject/Roomlulala/test/valueTopage/btn.js
@@ -3,7 +3,10 @@ const Mbtn = document.querySelector('.minus');
 
 const value = document.querySelector('span');
 
-let count = 0;
+window.localStorage.setItem('count', '0');
+let count = window.localStorage.getItem('count');
+
+
 
 Pbtn.addEventListener('click', () => { count++; value.innerText = count; console.log(count); });
 Mbtn.addEventListener('click', () => { count--; value.innerText = count; console.log(count); })
