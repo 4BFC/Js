022c06550fe63495a87c06fa2093eacc40489a85
4BFC <4bee.code@gmail.com>
2023-12-14 21:47:11 +0900

    edited files

---
+++ Js_project/mini/StudentGrade_mgr_sys/js/student_List.js
@@ -0,0 +1,27 @@
+const a = {
+  name: "전시은",
+  math: 50,
+  eng: 70,
+  kor: 80,
+}
+
+const b = {
+  name: "김지혜",
+  math: 70,
+  eng: 80,
+  kor: 90,
+}
+
+const c = {
+  name: "안유진",
+  math: 60,
+  eng: 50,
+  kor: 100,
+}
+
+const d = {
+  name: "이영지",
+  math: 50,
+  eng: 100,
+  kor: 90,
+}
