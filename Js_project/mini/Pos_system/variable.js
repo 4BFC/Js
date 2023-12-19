a4ec171a1f05ce354796e1bf6ae5c5e26f9894f3
4BFC <4bee.code@gmail.com>
2023-12-20 02:33:03 +0900

    edited files

--- Js_project/mini/Pos_system/variable.js
+++ Js_project/mini/Pos_system/variable.js
@@ -1,7 +1,7 @@
 export const menu_btn = document.querySelector('.Menu_btn')
 export const view_port = document.querySelector('.view_port')
 
-export const create_input = () => document.createElement('input')
+export const create_tag = (tagName) => document.createElement(tagName)
 //=>insert
 
 console.log("import variable.js")
