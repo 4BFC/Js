442031bf3adca8e57db4cc7e92c02bd209b18001
4BFC <4bee.code@gmail.com>
2024-01-11 18:35:11 +0900

    update

---
+++ js_Storage/mouse_position/main.js
@@ -0,0 +1,5 @@
+function mouse_tracking(event) {
+  // console.log(`pageX : ${event.pageX} pageY : ${event.pageY}`)
+  console.log(`clientX : ${event.clientX} clientY : ${event.clientY}`)
+}
+window.addEventListener('mousemove', mouse_tracking())
